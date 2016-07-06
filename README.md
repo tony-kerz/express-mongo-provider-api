f1714f638a5e4abd3b34d75ac959369d46bd72b8
Tony L. Kerz <anthony.kerz@gmail.com>
2016-07-05 21:41:07 -0400

    [providers] intro denorm

--- README.md
+++ README.md
@@ -4,4 +4,12 @@ this is an [express-js](http://expressjs.com/) app intended to serve up data ing
 
 ## tech notes
 
-this app is atypical in that it is using normalized collections and the [$lookup](https://docs.mongodb.com/manual/reference/operator/aggregation/lookup/) feature of mongo to "join" the data dynamically at runtime.
+this app shows two access paths:
+
+1. normalized collections and the [$lookup](https://docs.mongodb.com/manual/reference/operator/aggregation/lookup/) feature of mongo to "join" the data dynamically at runtime
+
+1. denormalized collection generated against normalized collections via aggregation pipeline using $lookup and [$out](https://docs.mongodb.com/manual/reference/operator/aggregation/out/)
+
+> - anecdotal observations with mongo 3.2.7 and a 1.5m record dataset are that denormalized collections are __way__ more performant (e.g. ~10ms v ~1500ms)
+> - query on joined collection involved a many-to-many (two calls to $lookup) and single field match on one of the joined collections
+> - mongo 3.3.6 introduces [performance improvement](https://jira.mongodb.org/browse/SERVER-21612), but that still only reduced the call to ~800ms
--- src/providers/routes.js
+++ src/providers/routes.js
@@ -1,20 +1,51 @@
 import express from 'express'
 import debug from 'debug'
-import {index} from './data'
+import _ from 'lodash'
+import {lookup, index} from './data'
 
 const dbg = debug('app:provider:routes')
 const router = express.Router()
 
-router.get('/', (req, res)=>{
+router.get('/lookup', (req, res)=>{
   dbg('get: req.query=%o', req.query)
-  index(
-    {
-      skip: parseInt(req.query.skip),
-      limit: parseInt(req.query.limit)
-    }
-  ).then((providers)=>{
+  lookup(getQuery(req), getOpts(req))
+  .then((providers)=>{
     res.send(providers)
   })
 })
+
+router.get('/', (req, res)=>{
+  dbg('get: req.query=%o', req.query)
+  index(getQuery(req), getOpts(req))
+  .then((providers)=>{
+    res.send(providers)
+  })
+})
+
+function getOpts(req) {
+  return _.reduce(
+    req.query,
+    (result, value, key)=>{
+      if (['skip', 'limit'].includes(key)) {
+        result[key] = parseInt(value)
+      }
+      return result
+    },
+    {}
+  )
+}
+
+function getQuery(req) {
+  return _.reduce(
+    req.query,
+    (result, value, key)=>{
+      if (!['skip', 'limit'].includes(key)) {
+        result[key] = value
+      }
+      return result
+    },
+    {}
+  )
+}
 
 export default router
